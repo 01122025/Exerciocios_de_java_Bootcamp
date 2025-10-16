be945f5924d5d2357cb3e03b4da8a76d526361fe
01122025 <pedro.asantos@telefonica.com>
2025-10-16 15:14:44 -0300

    Exercício 2 do dia 7
    Exercícios proposto no curso onde criamos um verificador de números, onde verifica se o numero é impar e qual categoria ele está.

--- Main.java
+++ Main.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
 
+// sistema de verificador de números impares.
 public class Main {
     public static void main(String[] args) {
-        Scanner imprimir1 =  new Scanner(System.in);
-        System.out.print("Imprima o primeiro numero: ");
-        int primeironumero = imprimir1.nextInt();
 
-        Scanner imprimir2 =  new Scanner(System.in);
-        System.out.print("Imprima o segundo numero: ");
-        int Segundonumero = imprimir2.nextInt();
+        Scanner inputDeEntrada = new Scanner(System.in);
+        System.out.print("Digite um numero: ");
+        int resultado =  inputDeEntrada.nextInt();
 
-        Scanner imprimir3 =  new Scanner(System.in);
-        System.out.print("Imprima o terceiro numero: ");
-        int terceironumero = imprimir3.nextInt();
+        if (resultado % 2 == 1 && resultado >2 && resultado < 5 ) {
+                System.out.println("Esse Valor é impar de categoria A está entre 2 e 5");
+        }
+        else if (resultado < 6 && resultado < 20 ) {
+                System.out.println("Esse valor é impar de categoria B e está entre 6 e 20");
+            }
+        else if ( resultado > 20){
+                System.out.println("Esse valor é impar de categoria C e está acima de 20");
+            }
+        else {
+                System.out.printf( resultado + " é par");
+        }
 
-        System.out.println("Esse é o conjunto de números: " + primeironumero+ " " + Segundonumero+ " "  + terceironumero );
+        }
+
     }
-}
