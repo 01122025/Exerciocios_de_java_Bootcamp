784704c33d40cabbfe13a1e778019582695245af
01122025 <pedro.asantos@telefonica.com>
2025-10-16 15:11:47 -0300

    Exercício 1  do dia 7
    Exercício proposto no curso onde o usuário digita um número inteiro, e sistema retorna o valor digitado.

--- Main.java
+++ Main.java
@@ -1,41 +1,19 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner imprimir1 =  new Scanner(System.in);
+        System.out.print("Imprima o primeiro numero: ");
+        int primeironumero = imprimir1.nextInt();
 
-    // Declaramos os parametros do cliente e puxamos o contrutor novaConta.
-       Clientes ID001 = new Clientes();
-       ID001.primeiroNome = "Pedro Henrique Alves dos Santos";
-       ID001.Idade = 26;
-       ID001.cpf = "123.456.789-00";
-       ID001.novaConta();
+        Scanner imprimir2 =  new Scanner(System.in);
+        System.out.print("Imprima o segundo numero: ");
+        int Segundonumero = imprimir2.nextInt();
 
+        Scanner imprimir3 =  new Scanner(System.in);
+        System.out.print("Imprima o terceiro numero: ");
+        int terceironumero = imprimir3.nextInt();
 
-        //Declaramos Variáveis publicas;
-       Car2 car2 = new Car2();
-       car2.marca = "GM";
-       car2.modelo= "Corsa frente Montana";
-       car2.ano = 2005;
-       car2.valorCarro = 125_000;
-       car2.mesesFinanciamento = 60;
-
-       car2.acrescimoPorcentagem();
-       car2.financiamento();
-
-
-       // declaramos novos valores a variaveis privadas.
-       Car car = new Car("Voksvagem","Fox",2020, 125_000, 72);
-        System.out.println("\n"+car.getMarca());
-        System.out.println(car.getModelo());
-        System.out.println(car.getano());
-        System.out.println(car.getValorCarro());
-        System.out.println(car.getMesesFinanciamento());
-
-        //Aqui alteramos o valores da mensalidade e valor do veiculo no  car
-        car.setValorCarro2(137_500);
-        car.setMesesFinanciamento2(72);
-
-        // aqui declarmos a construtor acrescimo e finaciamento.
-        car.acrescimoPorcentagem2();
-        car.financiamento2();
+        System.out.println("Esse é o conjunto de números: " + primeironumero+ " " + Segundonumero+ " "  + terceironumero );
     }
 }
